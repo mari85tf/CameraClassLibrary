43745b98c6e5271e3911bb983d04efa53d7b8045
Maria Nijar <r0830560@student.vives.be>
2022-11-28 13:30:03 +0100

    Add project files.

---
+++ CameraClassLibrary/Camera.cs
@@ -0,0 +1,11 @@
+﻿namespace CameraClassLibrary
+{
+    public class Camera
+    {
+        public int Id { get; set; }
+        public int PictureId { get; set; }
+
+        public DateTime Date { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
